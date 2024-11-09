1f04c89506818f3bdf00fd3e8749615585c0fbed
Augusto Fernandez <augusfernandez23@gmail.com>
2024-11-09 12:03:45 -0300

    Se hizo boiler plate de Electron

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 import { app, BrowserWindow } from "electron";
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const createWindow = () => {
     const mainWindow = new BrowserWindow({
-        title:"Generador de Convenios v0.1",
+        title: "Generador de Convenios v0.1",
         width: 800,
-        height: 600
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
     });
 
     mainWindow.loadFile('index.html');
@@ -16,10 +24,10 @@ const main = async () => {
         createWindow();
 
         app.on('window-all-closed', () => {
-            if (process.platform !== 'darwin'){
+            if (process.platform !== 'darwin') {
                 app.quit();
             }
-        })
+        });
     } catch (error) {
         console.error("Error al inicializar la aplicación:", error);
     }
