2a91ceaff18ebce582564df768513a83386af62d
Augusto Fernandez <augusfernandez23@gmail.com>
2024-11-19 16:39:10 -0300

    Se agrego Tailwind al proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/ui/**/*.{js,jsx,ts,tsx}",
+    "./dist/**/*.html", 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
