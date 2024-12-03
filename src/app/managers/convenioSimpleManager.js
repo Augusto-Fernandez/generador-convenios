edb524f9bed062be6c0500c046121eb1cc90450c
Augusto Fernandez <augusfernandez23@gmail.com>
2024-12-03 16:52:18 -0300

    Se soluciono error al justificar texto

--- src/app/managers/convenioSimpleManager.js
+++ src/app/managers/convenioSimpleManager.js
@@ -5,16 +5,16 @@ import { getLogoPath } from "../pathResolver.js";
 
 class ConvenioSimpleManager {
     constructor(data){
-        //ancho de una hoja A4
-        this.pageWidth = 595.28;
-        //alto de una hoja A4
-        this.pageHeight = 841.89;
-        this.margin = 50;
-        this.lineSpacing = 1.0;
+        //ancho de una hoja tamaño carta
+        this.pageWidth = 612;
+        //alto de una hoja tamaño carta
+        this.pageHeight = 792;
+        this.margin = 28.35;
+        this.lineSpacing = 1.75;
         //ancho máximo del texto
-        //es el ancho de la pagina A4 menos los dos margenes
+        //es el ancho de la pagina tamaño carta menos los dos margenes
         this.textMaxWidth = this.pageWidth - this.margin * 2;
-        //es el alto de la pagina A4 menos los dos margenes
+        //es el alto de la pagina tamaño carta menos los dos margenes
         this.textMaxHeight = this.pageHeight - this.margin * 2;
         //pdf creado
         this.pdf;
@@ -43,13 +43,13 @@ class ConvenioSimpleManager {
         //equivale a un margen entre la imagen y el titulo de 0.5cm en DPI
         const imgMargin = 14.175;
 
-        //la posición x de la imagen es el ancho de la hoja A4 menos el ancho de la imagen divido 2 para encontrar el punto medio
+        //la posición x de la imagen es el ancho de la hoja tamaño carta menos el ancho de la imagen divido 2 para encontrar el punto medio
         const imageX = (this.pageWidth - imageWidth) / 2;
-        //la posición y de la imagen es el alto de la hoja A4 menos el alto de la imagen menos el margen entre la imagen y el texto
+        //la posición y de la imagen es el alto de la hoja tamaño carta menos el alto de la imagen menos el margen entre la imagen y el texto
         const imageY = this.pageHeight - imageHeight - imgMargin;
         this.page.drawImage(pngImage, { x: imageX, y: imageY, width: imageWidth, height: imageHeight });
 
-        //ahora establece currentY como la altura de la página menos el margen superior menos el tamaño y menos el margen entre la imagen y el texto
+        //ahora establece currentTopMargin como la altura de la página menos el margen superior menos el tamaño y menos el margen entre la imagen y el texto
         this.currentTopMargin = imageY - imgMargin;
     };
 
@@ -70,26 +70,36 @@ class ConvenioSimpleManager {
             font: titleFont 
         });
 
-        //mueve currenY abajo del margen de title
-        this.currentTopMargin -= titleMargin;
+        //mueve currentTopMargin abajo del margen de title
+        this.currentTopMargin -= titleFontSize + titleMargin;
     };
 
     //Se encarga en dividir el texto en lineas que estén dentro del textMaxWidth
-    splitTextIntoLinesWithNewlines = async (fontSize) => {
+    //devuelve un array que contiene subarray que tienen el string del ancho del textMaxWidth
+    splitTextIntoLines = async (fontSize) => {
         this.font = await this.pdf.embedFont(StandardFonts.Helvetica);
-        //divide en rawLines respetando los saltos de línea que ya existen
+        const rawLines = this.body.split(/\r?\n/);
+        //divide el texto en lineas de acuerdo a los saltos de lineas
         /*Ej:
-            Línea uno con texto
-            Línea dos
+            Línea uno con texto.
+            Línea dos.
 
             queda:
-            rawLines = ["Línea uno con texto", "Línea dos"];
+            rawLines = ["Línea uno con texto.", "Línea dos."];
         */
-        const rawLines = this.body.split("\n");
         const formattedLines = [];
 
         //itera por cada linea
-        rawLines.forEach((rawLine) => {
+        rawLines.forEach((rawLine, index) => {
+            //con trim() saca los espacios en blanco del principio y final de rawLine
+            //si rawLine es una linea vacia
+            if (rawLine.trim() === "") {
+                //pushea una linea vacia
+                formattedLines.push("");
+                //corta ejecución
+                return;
+            }
+
             //divide en cada rawline en un array de palabras individuales borrando los espacios
             const words = rawLine.split(" ");
             //va a guardar la linea de texto ajustada
@@ -127,6 +137,13 @@ class ConvenioSimpleManager {
             if(currentLine){
                 formattedLines.push(currentLine);
             }
+
+            //antes de pasar a la siguiente linea
+            //se fija que la posición donde se está sea dentro del rango del array de lineas
+            //agrega un linea vacia al final de cada parrafo
+            if (index < rawLines.length - 1) {
+                formattedLines.push("");
+            }
         });
 
         //devuelve el array de las lineas formateadas
@@ -136,13 +153,14 @@ class ConvenioSimpleManager {
     //devuelve un objeto con la linea que se le pasa y la propiedad spacings que 
     //tiene los espacios que hacen falta para justificar la linea
     justifyLine = (line, fontSize) => {
-        //hace una array de palabras
-        const words = line.split(" ");
-        if (words.length === 1) {
-            // si solo hay una palabra, no se justifica
-            //devuelve spacings vacio
+        //si la linea termina en un salto de linea, devuelve un array vacío para que
+        //no se justifique la última linea del parrafo
+        if (line.endsWith(".\n")) {
             return { line, spacings: [] };
         }
+
+        //hace una array de palabras
+        const words = line.split(" ");
 
         //suma el tamaño de cada palabra que la calcula con widthOfTextAtSize
         const textWidth = words.reduce((widthAccumulator, word) => {
@@ -153,6 +171,7 @@ class ConvenioSimpleManager {
         const remainingSpace = this.textMaxWidth - textWidth;
         //cuenta la cantidad de espacios que hay tomando el length de words - 1 dado que la primera palabra no tiene un espacio anterior
         const spaceCount = words.length - 1; // Espacios entre palabras
+        
         //el espacio extra para justificar es el espacio sobrante dividido entra la cantidad de espacios
         const extraSpacing = remainingSpace / spaceCount;
 
@@ -166,13 +185,13 @@ class ConvenioSimpleManager {
 
     //calcula el tamaño máximo de la fuente para que entre dentro de una página
     calculateFontSize = async () => {  
-        let fontSize = 12;
+        let fontSize = 7;
 
         //se usa while porque va a probar cual tamaño de fuente corresponde
         //con cada iteración, baja el tamaño de la fuente
         while (fontSize > 1) {
             //divide al texto en lineas que usan el tamaño de la fuente
-            const lines = await this.splitTextIntoLinesWithNewlines(fontSize);
+            const lines = await this.splitTextIntoLines(fontSize);
             //establece la altura del texto como la cantidad de lineas por la multiplicación del tamaño de la fuente y el interlineado
             const textHeight = lines.length * (fontSize * this.lineSpacing);
             //si la altura del texto es menor o igual a la altura máxima de la pagina
@@ -191,43 +210,78 @@ class ConvenioSimpleManager {
         //calcula el tamaño de la fuente
         const fontSize = await this.calculateFontSize();
         //divide el texto en lineas de acuerdo al tamaño de fuente que corresponde
-        const lines = await this.splitTextIntoLinesWithNewlines(fontSize);
+        const lines = await this.splitTextIntoLines(fontSize);
 
-        //itera cada linea
-        lines.forEach(async (line) => {
-            //verifica si el margen superior está por encima del margen inferior
+        for (let i = 0; i < lines.length; i++) {
+            const line = lines[i];
+    
+            //con trim() saca los espacios en blanco del principio y final de la linea
+            //si line es una linea vacia
+            if (line.trim() === "") {
+                //hace un espacio entre vacio entre los parrafos
+                //el cual va a ser de la mitad del tamaño de la fuente por el interlineado
+                this.currentTopMargin -= (fontSize / 2) * this.lineSpacing;
+                //corta el la iteración del for con la linea y pasa a la próxima
+                continue;
+            }
+    
+            //valida si el margen superior está sobre el inferior
             if (this.currentTopMargin < this.margin) {
-                //si el margen superior está por debajo del margen inferior
-                //lo reinicia
+                //si el margen superior está por debajo del inferior
+                //reinicia la altura del margen superior
                 this.currentTopMargin = this.pageHeight - this.margin;
-                //crea una nueva pagina con tamaño de hoja A4
+                //crea una nueva pagina
                 this.page = this.pdf.addPage([this.pageWidth, this.pageHeight]);
             }
+    
+            //determinar si el la última linea el parrafo
+            let isLastLineOfParagraph = false;
 
-            //obtiene el array de espacios para justicar la linea
-            const { spacings } = this.justifyLine(line, fontSize);
-            //hace la linea en un array de palabras
-            const words = line.split(" ");
-            //establece como principio de la linea el margen izquierdo
-            let x = this.margin;
-
-            //recorre las palabras de la linea. Index es la posición actual en el array
-            words.forEach((word, index) => {
-                //escribe la palabra en la pagina
-                this.page.drawText(word, { x, y: this.currentTopMargin, size: fontSize, font: this.font });
-                //mueve x a la derecha haciendo un espacio normal
-                x += this.font.widthOfTextAtSize(word, fontSize);
-
-                //si el index es menor al tamaño del array de espacios
-                if (index < spacings.length) {
-                    //agrega el espacio extra correspondiente al indice de la palabra donse se está
-                    x += spacings[index];
-                }
-            });
-
+            //si la posición del bucle es mayor a la cantida de lineas que hay
+            //o si la linea siguiente a la que se está ahora es una linea vacía
+            if(i === lines.length - 1 || lines[i + 1].trim() === ""){
+                isLastLineOfParagraph = true;
+            };
+    
+            //escribe el texto sin justificar
+            if (isLastLineOfParagraph) {
+                //establece como principio de la linea el margen izquierdo
+                let x = this.margin;
+                //divide la linea en un array de palabras sin espacios
+                const words = line.split(" ");
+                words.forEach((word) => {
+                    //escribre la palabra
+                    this.page.drawText(word, { x, y: this.currentTopMargin, size: fontSize, font: this.font });
+                    //mueve x hacía la derecha, la distancia es el ancho de la palabra que escribió mas un espacio común
+                    //de esta forma la próxima palabra se escribe después del espacio que sigue a la palabra anterior
+                    x += this.font.widthOfTextAtSize(word, fontSize) + this.font.widthOfTextAtSize(" ", fontSize);
+                });
+            //justifica el texto
+            } else {
+                //obtiene los espacios para que la linea esté justificada
+                const { spacings } = this.justifyLine(line, fontSize);
+                //divide la linea en un array de palabras sin espacios
+                const words = line.split(" ");
+                //establece como principio de la linea el margen izquierdo
+                let x = this.margin;
+    
+                words.forEach((word, index) => {
+                    //escribre la palabra
+                    this.page.drawText(word, { x, y: this.currentTopMargin, size: fontSize, font: this.font });
+                    //mueve x a la derecha haciendo un espacio normal
+                    x += this.font.widthOfTextAtSize(word, fontSize);
+    
+                    //si el index es menor al tamaño del array de espacios
+                    if (index < spacings.length) {
+                        //agrega el espacio extra correspondiente al indice de la palabra donse se está
+                        x += spacings[index];
+                    }
+                });
+            }
+    
             //actualiza currentY restandole el tamaño de la fuente de la Linea por el interlineado
             this.currentTopMargin -= fontSize * this.lineSpacing;
-        });
+        }
     };
 
     createPdf = async () =>{
