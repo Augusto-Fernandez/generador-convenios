fcc206888e1fbe8cffeba5c1e918070f79da0f5c
Augusto Fernandez <augusfernandez23@gmail.com>
2025-01-01 16:28:04 -0300

    Se aplico css

--- src/ui/components/Menu.jsx
+++ src/ui/components/Menu.jsx
@@ -4,17 +4,19 @@ import Header from "./Header.jsx";
 
 const Menu = () => {
   return (
-    <div>
+    <>
       <Header title={"Generador de Convenios"}/>
-      <ul>
-        <li>
-          <Link to="/convenio-simple">Convenio Simple</Link>
-        </li>
-        <li>
-          <Link to="/convenio-honorarios">Convenio con Honorarios</Link>
-        </li>
-      </ul>
-    </div>
+      <div className="flex flex-col items-center justify-center">
+        <ul className="mt-8 p-3 border border-gray-200 rounded-md">
+          <li>
+            <Link to="/convenio-simple">Convenio Simple</Link>
+          </li>
+          <li>
+            <Link to="/convenio-honorarios">Convenio con Honorarios</Link>
+          </li>
+        </ul>
+      </div>
+    </>
   )
 }
 
